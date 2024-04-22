59f1bc5815b8801a973afd0d8f3d1df1209057d6
RedHammer <rassadin.rp@gmail.com>
2024-04-22 19:29:19 +0400

    добавил всплывающий поисковик

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-function showPopup() {
-    var popup = document.getElementById("popup");
+function showPopup(str) {
+    var popup = document.getElementById(str);
     popup.style.display = "block";
 }
 
-function hidePopup() {
-    var popup = document.getElementById("popup");
+function hidePopup(str) {
+    var popup = document.getElementById(str);
     popup.style.display = "none";
+}
+
+function SearchBox() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("UL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+      
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
