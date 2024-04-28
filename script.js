67a7d49b79c72a472a8252b037f8208de713caf2
RedHammer <rassadin.rp@gmail.com>
2024-04-28 18:00:23 +0400

    Осталось только сделать сайты и поисковик

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ function showPopup(str) {
     var popup = document.getElementById(str);
     popup.style.display = "block";
 }
+function showFlexPopup(str) {
+    var popup = document.getElementById(str);
+    popup.style.display = "flex";
+}
 
 function hidePopup(str) {
     var popup = document.getElementById(str);
     popup.style.display = "none";
 }
 
+
 function SearchBox() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("search");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("UL");
+    ul = document.getElementById("search_list");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
